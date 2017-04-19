96d9b7429eb11628e9bf8d019be384a01fa10aae
Jonghwan Choi <descartes408@gmail.com>
2017-04-19 22:09:10 +0900

    Update CPR.py

--- CPR.py
+++ CPR.py
@@ -1,3 +1,8 @@
+""" information
+
+170419: This code is written by Jonghwan Choi
+
+"""
 from __future__ import print_function
 import numpy as np
 import pickle
@@ -501,29 +506,3 @@ def load_data():
 	f.close()
 	
 	return result
-
-if __name__=="__main__":
-	examples = load_data()
-	
-	edgeList = examples['edgeList']
-	geneList = examples['geneList']
-	data_train = examples['data_BRCA']
-	label_train = examples['label_BRCA']
-	data_test = examples['data_GSE4922']
-	label_test = examples['label_GSE4922']
-	
-	# 3. generate CPR
-	print(">>> fitting...")
-	cpr = CPR()
-	cpr.setParam(dampingFactor=0.5, n_biomarkers=70)
-	cpr.fit(geneList, edgeList, data_train, label_train, randomState=1)
-	
-	print("The number of biomarkers: %d" % len(cpr.biomarkers))
-	cpr.getParam()
-	
-	auc = cpr.estimate(geneList, data_test, label_test, randomState=1)
-	print("AUC = %g" % auc)
-	
-	cpr.print_rankedGenes(5)
-	
-	cpr.print_biomarkers(5)
